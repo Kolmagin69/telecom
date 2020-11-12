fa130fa96204af5bf22263d16aee1ced0c025db9
Kolmagin69 <nikone.12.95@yandex.ru>
2020-11-12 12:41:28 +0300

    add PublisherApplication

---
+++ publisher/src/main/java/intech/com/PublisherApplication.java
@@ -0,0 +1,13 @@
+package intech.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherApplication.class, args);
+	}
+
+}
