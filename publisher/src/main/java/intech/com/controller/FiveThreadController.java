4002d8cc74a30304776ffdf976efed3f570e5400
Kolmagin69 <nikone.12.95@yandex.ru>
2020-11-17 17:17:40 +0300

    add logger

--- publisher/src/main/java/intech/com/controller/FiveThreadController.java
+++ publisher/src/main/java/intech/com/controller/FiveThreadController.java
@@ -6,11 +6,11 @@ import intech.com.exception.ThreadsRunningException;
 import intech.com.model.Message;
 import intech.com.model.ThreadFunction;
 import intech.com.model.ThreadList;
+import intech.com.repository.MessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,8 +19,13 @@ import java.net.URI;
 @Controller
 public class FiveThreadController implements ThreadController {
 
+    public static final Logger logger = LoggerFactory.getLogger(FiveThreadController.class);
+
     @Autowired
-    private MessageController messageController;
+    private MessageGenerator messageController;
+
+    @Autowired
+    private MessageRepository messageRepository;
 
     private ThreadList threadList;
 
@@ -28,7 +33,7 @@ public class FiveThreadController implements ThreadController {
 
     private ThreadList createThreadList() {
         final ThreadFunction threadFunction = () -> {
-            final Message newMassage = messageController.generatedMessage();
+            final Message newMassage = messageRepository.save(messageController.generatedMessage());
             final URI localhostURI = URI.create("http://localhost:8086/subscriber/post/message");
             final RestTemplate restTemplate = new RestTemplate();
             final HttpHeaders httpHeaders = new HttpHeaders();
@@ -36,6 +41,7 @@ public class FiveThreadController implements ThreadController {
             final String json = objectToStringJson(newMassage);
             HttpEntity<String> httpEntity = new HttpEntity<>(json, httpHeaders);
             restTemplate.exchange(localhostURI, HttpMethod.POST, httpEntity, Message.class);
+            logger.info("Generate and sent " + newMassage.toString() + " to subscriber");
         };
         return new ThreadList(5, threadFunction);
     }
@@ -44,6 +50,7 @@ public class FiveThreadController implements ThreadController {
         if (isThreadsRunning)
             throw new ThreadsRunningException("Threads are already running, only 5 threads can be run at a time!");
         threadList = createThreadList();
+        logger.info("Start pushing messages");
         for (Thread thread : threadList.getThreadList())
             thread.start();
         isThreadsRunning = true;
