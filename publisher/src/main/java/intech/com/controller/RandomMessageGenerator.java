04de5a659aaab7e612f8ffc0e75c4e6609fa02cf
Kolmagin69 <nikone.12.95@yandex.ru>
2020-11-20 13:42:30 +0300

    minor change

--- publisher/src/main/java/intech/com/controller/RandomMessageGenerator.java
+++ publisher/src/main/java/intech/com/controller/RandomMessageGenerator.java
@@ -5,21 +5,25 @@ import intech.com.model.Message;
 import org.springframework.stereotype.Controller;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Random;
 
 @Controller
 public class RandomMessageGenerator implements MessageGenerator {
 
     @Override
-    public Message generatedMessage() {
-        return new Message(getRandomInt(),
-                Action.getRandomAction(), 
-                new Timestamp(System.currentTimeMillis()));
+    public  Message generatedMessage() {
+        final Message message = new Message();
+        message.setMsisdn(getRandomInt());
+        message.setAction(Action.getRandomAction());
+        message.setTimestamp(System.currentTimeMillis());
+        return message;
+
     }
 
     private int getRandomInt() {
         final Random random = new Random();
-        int i = random.nextInt();
+        final int i = random.nextInt();
         return i >= 0 ? i : -1 * i;
     }
 
