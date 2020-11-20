f67701298a062d3faaccb513ad879681b7eb30f1
Kolmagin69 <nikone.12.95@yandex.ru>
2020-11-20 14:56:22 +0300

    added creating USER and DATABASE

--- publisher/src/main/resources/db.sql
+++ publisher/src/main/resources/db.sql
@@ -1,3 +1,6 @@
+CREATE USER publisher WITH PASSWORD 'pas123';
+CREATE DATABASE subscriber OWNER publisher;
+
 CREATE TABLE messages (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     msisdn int NOT NULL,
--- subscriber/src/main/resources/db.sql
+++ subscriber/src/main/resources/db.sql
@@ -1,3 +1,6 @@
+CREATE USER subscriber WITH PASSWORD 'pas123';
+CREATE DATABASE subscriber OWNER subscriber;
+
 CREATE TABLE purchases (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     msisdn int NOT NULL,
