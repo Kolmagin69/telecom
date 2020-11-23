5aeb05202b6aa4ef863f99811894b0d4b2ac4fb6
Kolmagin69 <nikone.12.95@yandex.ru>
2020-11-23 17:04:17 +0300

    remove

--- publisher/src/main/resources/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER publisher WITH PASSWORD 'pas123';
-CREATE DATABASE subscriber OWNER publisher;
-
-CREATE TABLE messages (
-    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    msisdn int NOT NULL,
-    action varchar(15) NOT NULL,
-    timestamp timestamp NOT NULL DEFAULT NOW()
-);
